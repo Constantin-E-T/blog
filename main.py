45b0bebfec2e602125323ff70c8419608db74848
Constantin ET <emiliantivlica@yahoo.com>
2021-10-05 19:05:42 +0100

    new db on heroku

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv("MY_SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("MY_SECRET_KEY")
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
                     base_url=None)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'qlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
