8635e9e51eb6854ecac2eed7412740c3af6526db
Constantin ET <emiliantivlica@yahoo.com>
2021-10-05 18:43:15 +0100

    update env file

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY_Blog")
+app.config['SECRET_KEY'] = os.getenv("MY_SECRET_KEY")
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
                     base_url=None)
